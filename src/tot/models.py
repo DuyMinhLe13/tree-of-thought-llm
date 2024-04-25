d77945d4b7a08953b34fa31dd8540afd478e8cac
DuyMinhLe13 <118045868+DuyMinhLe13@users.noreply.github.com>
2024-04-25 17:48:35 +0700

    Update models.py

--- src/tot/models.py
+++ src/tot/models.py
@@ -2,8 +2,8 @@ import os
 import google.generativeai as genai
 import time
 genai.configure(api_key=os.getenv("GEMINI_API_KEY", ""))
-model = genai.GenerativeModel('gemini-pro')
+llm_model = genai.GenerativeModel('gemini-pro')
 
 def gpt(prompt, model="gpt-4", temperature=0.7, max_tokens=1000, n=1, stop=None) -> list:
     time.sleep(0.5)
-    return [model.generate_content(prompt).text]
+    return [llm_model.generate_content(prompt).text]
